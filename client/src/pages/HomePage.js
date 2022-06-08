abe36e9974ad339c87267394fed0f3e708ac35a9
Wang Haoyang <haoyangw@gmail.com>
2022-06-08 19:47:42 +0800

    Load images in the background using an async function and show 'Loading...' until it's complete

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -12,14 +12,28 @@ const MainContainer = styled.div`
 
 function HomePage() {
   const [images, setImages] = useState([]);
+  const [ urlImgs, setUrlImgs ] = useState([]);
+
   useEffect(() => {
     axios
       .get(`${BACKEND_URL}/getItems`)
       .then((res) => {
         setImages(res.data);
+        binsToImgs(res.data);
       })
       .catch((err) => console.log(err, "error occured"));
   }, []);
+
+  async function binsToImgs(bins) {
+    var imgs = [];
+    bins.forEach((bin, index) => {
+      const binary = Buffer.from(bin.image.data.data);
+      const blob = new Blob([binary.buffer], {type: 'application/octet-binary'});
+      const url = URL.createObjectURL(blob);
+      imgs[index] = url;
+      setUrlImgs(imgs);
+    });
+  }
 
   return (
     <MainContainer>
@@ -27,11 +41,8 @@ function HomePage() {
       <div>Image upload</div>
       <div>
         { images ? 
-          (images.map((singleimage) => {
-            const binary = Buffer.from(singleimage.image.data.data);
-            const blob = new Blob([binary.buffer], {type: 'application/octet-binary'});
-            const url = URL.createObjectURL(blob);
-            return <ItemCard title={singleimage.name} image={url} description={singleimage.desc} />
+          (images.map((singleimage, index) => {
+            return <ItemCard title={singleimage.name} image={ urlImgs[index] } description={singleimage.desc} />
           })) : 
           "Loading..." 
         }
