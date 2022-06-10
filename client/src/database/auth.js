91b1c613df8d1e2d0b37b618417fd21220d31afc
Wang Haoyang <haoyangw@gmail.com>
2022-06-10 17:13:18 +0800

    Add hasUser and signUpUser functions to AuthProvider's context to facilitate accessing these backend functions from our frontend

--- client/src/database/auth.js
+++ client/src/database/auth.js
@@ -51,9 +51,52 @@ function useAuthProvider() {
         });
     };
 
+    const signUpUser = async (givenName, givenAge, givenUsername, givenPassword) => {
+        const isRegistered = await hasUser(givenUsername);
+        if (!isRegistered) {
+            const req = await fetch(`${BACKEND_URL}/signUpUser`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: givenName,
+                    age: givenAge,
+                    username: givenUsername,
+                    password: givenPassword
+                })
+            });
+            if (req.body.status === 'ok') {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    const hasUser = async (givenUsername) => {
+        const req = await fetch(`${BACKEND_URL}/hasUser`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: givenUsername
+            })
+        });
+
+        const data = await req.json();
+        if (data.status === 'ok') {
+            return data.hasUser;
+        }
+        return false;
+    }
+
     return {
+        hasUser,
         signInWithGoogle,
         signInUserPass,
+        signUpUser,
         signOut,
         user,
         isGoogleSignIn
