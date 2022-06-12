e59a2cd784e1a2101fc50cd252ed9340985819a3
Wang Haoyang <haoyangw@gmail.com>
2022-06-12 22:00:48 +0800

    Fix theming issues on the signin/signout page

--- client/src/components/SignInComps/AltSignInComp.js
+++ client/src/components/SignInComps/AltSignInComp.js
@@ -3,6 +3,18 @@ import { CentredTypo, WideBox } from "./AuthCard";
 import GoogleIcon from '@mui/icons-material/Google'
 import { useAuth } from "../../database/auth";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { theme } from "../Theme";
+
+const AltSignInDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    & .MuiLink-root {
+        color: ${theme.palette.accent.main};
+        text-decoration-color: ${theme.palette.accent.main};
+    }
+`;
 
 export default function AltSignInComp() {
     const { signInWithGoogle } = useAuth();
@@ -20,13 +32,13 @@ export default function AltSignInComp() {
     };
 
     return (
-        <>
+        <AltSignInDiv>
             <CentredTypo variant="body1">OR</CentredTypo>
-            <Button variant="contained" startIcon={<GoogleIcon />} onClick={ completeGoogleSignIn }>Sign in with Google</Button>
+            <Button variant="outlined" color="info" startIcon={<GoogleIcon />} onClick={ completeGoogleSignIn }>Sign in with Google</Button>
             <WideBox>
                 <Link component={Link} to="#">Can't log in?</Link>
                 <Link component={Link} to="#">Create an account</Link>
             </WideBox>
-        </>
+        </AltSignInDiv>
     );
 }
