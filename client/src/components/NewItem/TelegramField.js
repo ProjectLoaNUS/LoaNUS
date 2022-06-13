10e18904aba91efbfaf65c07d55f08bfd437d005
Wang Haoyang <haoyangw@gmail.com>
2022-06-13 21:42:01 +0800

    Add helpful suggested input inside Telegram input field on item request/listing card

---
+++ client/src/components/NewItem/TelegramField.js
@@ -0,0 +1,19 @@
+import { FormControl, InputLabel, OutlinedInput } from "@mui/material";
+
+export default function TelegramField(props) {
+    const { setTelegramHandle } = props;
+    const label = "Your Telegram handle";
+
+    return (
+        <FormControl required variant="outlined">
+            <InputLabel htmlFor="telegram">{ label }</InputLabel>
+            <OutlinedInput
+                required
+                id="telegram"
+                variant="outlined"
+                placeholder="@HelpfulNusStudent"
+                label={ label }
+                onChange={(event) => setTelegramHandle(event.target.value)} />
+        </FormControl>
+    );
+}
