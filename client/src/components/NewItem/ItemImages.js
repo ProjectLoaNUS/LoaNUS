b1f5617f538a415d21b6f5f62ff861feeadc9f1d
Wang Haoyang <haoyangw@gmail.com>
2022-06-14 15:36:51 +0800

    Add ItemImages component for user to add item images to an item listing

---
+++ client/src/components/NewItem/ItemImages.js
@@ -0,0 +1,93 @@
+import { Card, CardActionArea, CardActions, CardMedia, IconButton, Stack } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import styled from "styled-components";
+import { useRef } from "react";
+import { theme } from "../Theme";
+
+const HiddenInput = styled.input`
+    display: none;
+`;
+const ImageStack = styled(Stack)`
+    display: flex;
+    flex-direction: row;
+    align-items: start;
+    align-self: stretch;
+    column-gap: 1ch;
+    overflow-x: auto;
+    padding: 1ch;
+    border: 1px solid ${theme.palette.primary.main};
+    border-radius: 4px;
+`;
+const ImageCard = styled(Card)`
+    display: flex;
+    flex: 0 0 auto;
+    flex-direction: column;
+    align-items: stretch;
+    width: 30vw;
+    height: 30vh;
+    & .MuiCardActionArea-root {
+        min-width: 100%;
+        min-height: 100%;
+    }
+    & .MuiCardMedia-root {
+        display: flex;
+        flex: 1 1 auto;
+        align-items: center;
+        justify-content: center;
+    }
+    & .MuiCardActions-root {
+        display: flex;
+        flex: 0 1 auto;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+`;
+
+export default function ItemImages(props) {
+    const { images, setImages } = props;
+    const chosenImg = useRef(null);
+    const maxNumOfImages = 6;
+
+    const handleChooseImg = () => {
+        chosenImg.current.click();
+    }
+
+    const onImgChosen = (event) => {
+        const url = URL.createObjectURL(event.target.files[0]);
+        setImages([...images, url]);
+    }
+
+    return (
+        <ImageStack direction="row">
+            { images.map((image, index) => {
+                return (
+                    <ImageCard key={index}>
+                        <CardMedia 
+                          component="img"
+                          height="auto"
+                          image={image}
+                          alt="Item image" />
+                        <CardActions>
+                            <IconButton size="small"><ArrowLeftIcon /></IconButton>
+                            <IconButton size="small"><DeleteIcon /></IconButton>
+                            <IconButton size="small"><ArrowRightIcon /></IconButton>
+                        </CardActions>
+                    </ImageCard>
+                );
+            })}
+            { (images.length < maxNumOfImages) &&
+                <ImageCard>
+                    <CardActionArea onClick={handleChooseImg}>
+                        <CardMedia>
+                            <AddCircleIcon />
+                        </CardMedia>
+                    </CardActionArea>
+                </ImageCard>
+            }
+            <HiddenInput type="file" id="choose-image" accept="image/gif,image/jpeg,image/png,image/svg+xml" onChange={onImgChosen} ref={chosenImg} /> 
+        </ImageStack>
+    );
+}
