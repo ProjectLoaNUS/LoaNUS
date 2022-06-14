94981c999a53048c1eec7140231221bf49a98810
YongbinWang <yongbin_01@hotmail.com>
2022-06-14 16:31:51 +0800

    Set up email verification

--- server/index.js
+++ server/index.js
@@ -8,8 +8,12 @@ const UserModel = require("./models/Users");
 const ItemModel = require("./models/Items");
 const cors = require("cors");
 const { request } = require("http");
-const bcrypt = require('bcryptjs');
+const sgMail = require("@sendgrid/mail");
+const crypto = require("crypto");
+const bcrypt = require("bcryptjs");
+const { Router } = require("express");
 const PORT = process.env.PORT || 3001;
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
@@ -17,20 +21,22 @@ mongoose.connect(
   "mongodb+srv://loanus123:loanushyyb123@loanus-database.csjkq.mongodb.net/loanusdatabase?retryWrites=true&w=majority"
 );
 
+sgMail.setApiKey(process.env.API_KEY);
+
 app.post("/api/hasUser", async (req, res) => {
   const givenEmail = req.body.email;
   const user = await UserModel.findOne({
-    email: givenEmail
+    email: givenEmail,
   });
   if (!user) {
     return res.json({
-      status: 'ok',
-      hasUser: false
+      status: "ok",
+      hasUser: false,
     });
   }
   return res.json({
-    status: 'ok',
-    hasUser: true
+    status: "ok",
+    hasUser: true,
   });
 });
 
@@ -39,38 +45,83 @@ app.post("/api/login", async (req, res) => {
     SUCCESS: 0,
     INVALID_PASSWORD: 1,
     NO_SUCH_USER: 2,
-    UNKNOWN: 3
-  }
+    UNKNOWN: 3,
+  };
   const givenUser = await UserModel.findOne({
-    email: req.body.email
+    email: req.body.email,
   });
   if (!givenUser) {
-    return res.json({status: 'error', errorCode: signInResultCodes.NO_SUCH_USER, error: `User {givenUser.name} doesn't exist`});
+    return res.json({
+      status: "error",
+      errorCode: signInResultCodes.NO_SUCH_USER,
+      error: `User {givenUser.name} doesn't exist`,
+    });
   }
   await bcrypt.compare(req.body.password, givenUser.password, (err, result) => {
     if (err) {
-      return res.json({status: 'error', errorCode: signInResultCodes.UNKNOWN, error: err});
+      return res.json({
+        status: "error",
+        errorCode: signInResultCodes.UNKNOWN,
+        error: err,
+      });
     }
     if (result) {
-      return res.json({status: 'ok', user: givenUser});
+      return res.json({ status: "ok", user: givenUser });
     }
-    return res.json({status: 'error', errorCode: signInResultCodes.INVALID_PASSWORD, error: `Invalid password for {givenUser.name}`});
+    return res.json({
+      status: "error",
+      errorCode: signInResultCodes.INVALID_PASSWORD,
+      error: `Invalid password for {givenUser.name}`,
+    });
   });
 });
 
 app.post("/api/signUpUser", async (req, res) => {
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  await UserModel.create({
+  var newUser = new UserModel({
     name: req.body.name,
     age: req.body.age,
     email: req.body.email,
-    password: hashedPassword
-  }, (err) => {
+    emailToken: crypto.randomBytes(64).toString("hex"),
+    isVerified: false,
+    password: hashedPassword,
+  });
+  await newUser.save({}, (err) => {
     if (err) {
-      return res.json({status: 'error', error: err});
+      return res.json({ status: "error", error: err });
     }
-  })
-  return res.json({status: 'ok'});
+  });
+
+  const msg = {
+    to: req.body.email,
+    from: "yongbin0162@gmail.com",
+    subject: "LoaNUS - Verify your email",
+    text: `Thanks for signing up for our site! Please copy and paste the address to verify your account. http://${req.headers.host}/verify-email?token=${newUser.emailToken}`,
+  };
+
+  sgMail.send(msg, function (err, info) {
+    if (err) {
+      console.log("Email Not Sent");
+    } else {
+      console.log("Email Sent Success");
+    }
+  });
+  return res.json({ status: "ok" });
+});
+
+// Email verification route
+app.get("/verify-email", async (req, res, next) => {
+  try {
+    const user = await UserModel.findOne({ emailToken: req.query.token });
+    if (!user) {
+      console.log("error", "Token is invalid");
+    }
+    user.emailToken = null;
+    user.isVerified = true;
+    await user.save();
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 const storage = multer.memoryStorage();
@@ -80,7 +131,7 @@ const upload = multer({
   limits: { fieldsize: 1024 * 1024 * 3 },
 });
 app.get("/api/getItemTexts", (req, res) => {
-  ItemModel.find({}, ['name', 'desc'], null, (err, items) => {
+  ItemModel.find({}, ["name", "desc"], null, (err, items) => {
     if (err) {
       console.log(err);
       res.status(500).send("An error occurred", err);
@@ -90,7 +141,7 @@ app.get("/api/getItemTexts", (req, res) => {
   });
 });
 app.get("/api/getItemImages", (req, res) => {
-  ItemModel.find({}, ['image'], null, (err, items) => {
+  ItemModel.find({}, ["image"], null, (err, items) => {
     if (err) {
       console.log(err);
       res.status(500).send("An error occurred", err);
@@ -99,26 +150,30 @@ app.get("/api/getItemImages", (req, res) => {
     }
   });
 });
-app.post("/api/item-upload", upload.single("image"), (request, response, next) => {
-  console.log(request.file);
-  const obj = {
-    name: request.body.name,
-    desc: request.body.description,
-    image: {
-      data: request.file.buffer,
-      contentType: request.file.mimetype,
-    },
-  };
-  ItemModel.create(obj, (err, item) => {
-    if (err) {
-      console.log(err);
-    } else {
-      item.save();
-    }
-  });
+app.post(
+  "/api/item-upload",
+  upload.single("image"),
+  (request, response, next) => {
+    console.log(request.file);
+    const obj = {
+      name: request.body.name,
+      desc: request.body.description,
+      image: {
+        data: request.file.buffer,
+        contentType: request.file.mimetype,
+      },
+    };
+    ItemModel.create(obj, (err, item) => {
+      if (err) {
+        console.log(err);
+      } else {
+        item.save();
+      }
+    });
 
-  response.send("Upload success");
-});
+    response.send("Upload success");
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${PORT}`);
