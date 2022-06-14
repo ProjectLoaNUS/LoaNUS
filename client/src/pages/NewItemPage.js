c4bfd394e6ee7f6f5a97303e241810937b8465ac
Wang Haoyang <haoyangw@gmail.com>
2022-06-15 01:50:58 +0800

    Remove old item image upload form from item request/listing page now that we have the proper UI working

--- client/src/pages/NewItemPage.js
+++ client/src/pages/NewItemPage.js
@@ -1,6 +1,4 @@
 import styled from "styled-components";
-import { useState } from "react";
-import { BACKEND_URL } from "../database/const";
 import NewItemCard from "../components/NewItem/NewItemCard";
 
 const MainContainer = styled.div`
@@ -13,47 +11,9 @@ const MainContainer = styled.div`
 `;
 
 function NewItemPage() {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [selectedFile, setSelectedFile] = useState();
-
-  const changeHandler = (event) => {
-    setSelectedFile(event.target.files[0]);
-  };
-  const handleUpload = (event) => {
-    event.preventDefault();
-    const formData = new FormData();
-
-    formData.append("name", name);
-    formData.append("description", description);
-    formData.append("image", selectedFile);
-    fetch(`${BACKEND_URL}/api/item-upload`, {
-      method: "POST",
-      body: formData,
-    })
-      .then((result) => {
-        console.log("File Upload successful");
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  };
 
   return (
     <MainContainer>
-      <div>Create Request Page</div>
-      <form onSubmit={handleUpload}>
-        <input
-          type="text"
-          onChange={(event) => setName(event.target.value)}
-        ></input>
-        <input
-          type="text"
-          onChange={(event) => setDescription(event.target.value)}
-        ></input>
-        <input type="file" onChange={changeHandler}></input>
-        <button type="submit">Upload</button>
-      </form>
       <NewItemCard />
     </MainContainer>
   );
