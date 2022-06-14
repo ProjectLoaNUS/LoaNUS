67f8df17f82c20e320d7192527f45ce69b436971
Wang Haoyang <haoyangw@gmail.com>
2022-06-15 01:46:56 +0800

    Define new backend endpoint /api/items/addRequest for item requests from the frontend

---
+++ server/routes/items/itemRequests.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const ItemRequestsModel = require("../../models/ItemRequests");
+
+router.post("/addRequest", async (req, res) => {
+    console.log(req);
+    const obj = {
+      title: req.body.title,
+      description: req.body.description,
+      location: req.body.location,
+      telegram: req.body.telegram
+    };
+    ItemRequestsModel.create(obj, (err, request) => {
+      if (err) {
+        console.log(err);
+        return res.json({status: 'error'});
+      } else {
+        request.save();
+        return res.json({status: 'ok'});
+      }
+    });
+});
+
+module.exports = router;
