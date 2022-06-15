8e021689bf5c000d63cf74fa996b186149a3fe39
Wang Haoyang <haoyangw@gmail.com>
2022-06-15 22:39:42 +0800

    Remove dropdown menu upon clicking create button of nav bar(since both item requests and listings are on the same page now) and only direct user to that page if logged in

--- client/src/components/NavBar/CreateBtn.js
+++ client/src/components/NavBar/CreateBtn.js
@@ -1,48 +1,33 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { IconButton, Menu } from '@mui/material';
-import { MenuItem } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { IconButton } from '@mui/material';
 import { AddCircleOutline } from '@mui/icons-material';
 import { ShrinkDiv } from '../FlexDiv';
 import styled from 'styled-components';
 import { theme } from '../Theme';
+import { useAuth } from '../../database/auth';
+import { NEW_ITEM } from '../../pages/routes';
 
 const ContrastIconBtn = styled(IconButton)`
     color: ${theme.palette.primary.contrastText};
 `;
 
 export default function CreateBtn() {
-    const [ anchorEl, setAnchorEl ] = useState(null);
-    const open = Boolean(anchorEl);
+    const { user } = useAuth();
+    const navigate = useNavigate();
     
     const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    }
-    const handleClose = () => {
-        setAnchorEl(null);
+        if (user) {
+            navigate(NEW_ITEM);
+        }
     }
 
     return (
         <ShrinkDiv>
             <ContrastIconBtn 
               id="create-btn"
-              aria-controls={'create-menu'}
-              aria-haspopup="true"
               onClick={handleClick}>
                   <AddCircleOutline />
               </ContrastIconBtn>
-            <Menu
-              id="create-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{
-                  'arial-labelledby': 'basic-button'
-              }}
-            >
-                <MenuItem component={ Link } to='/new-item'>List Item</MenuItem>
-                <MenuItem component={ Link } to='/new-item'>Request Item</MenuItem>
-            </Menu>
         </ShrinkDiv>
     );
 }
