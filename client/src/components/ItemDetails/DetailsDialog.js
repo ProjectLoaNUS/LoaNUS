fb0e28e990eeca2e3c77f58feffe825621e34482
Wang Haoyang <haoyangw@gmail.com>
2022-06-22 17:42:57 +0800

    Add Dialog component that displays item listing details

---
+++ client/src/components/ItemDetails/DetailsDialog.js
@@ -0,0 +1,60 @@
+import { Dialog, DialogContent, DialogTitle, IconButton, Link, Typography } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+import styled from "styled-components";
+import ImageList from "./ImageList";
+
+const DialogContainer = styled(DialogContent)`
+    display: flex;
+    flex-direction: column;
+    gap: 0.5ch;
+    align-items: stretch;
+    padding: 1rem;
+`;
+
+export default function DetailsDialog(props) {
+    const { date, userName, title, isRequest, category, description, location, telegram, imageUrls, deadline, open, setOpen } = props;
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const onClickChat = (event) => {
+
+    }
+
+    return (
+        <Dialog open={open} onClose={handleClose} scroll="paper">
+            <DialogTitle>
+                Item {isRequest ? "Request" : "Listing"}
+                <IconButton
+                  aria-label="close"
+                  onClick={handleClose}
+                  sx={{
+                    position: 'absolute',
+                    right: 8,
+                    top: 8,
+                    color: 'rgba(0, 0, 0, 0.87)',
+                  }}
+                >
+                    <CloseIcon />
+                </IconButton>
+            </DialogTitle>
+            <DialogContainer>
+                <Typography variant="h3" align="left">{ title }</Typography>
+                {imageUrls && <ImageList imageUrls={imageUrls} />}
+                <Typography variant="caption" align="left">{date} by {userName}</Typography>
+                <Typography variant="caption" align="left">In {category}</Typography>
+                <Typography variant="body1" align="left">{description}</Typography>
+                {deadline && (
+                    <>
+                        <Typography variant="subtitle1" align="left">Return deadline</Typography>
+                        <Typography variant="caption" align="left">{deadline}</Typography>
+                    </>
+                )}
+                <Typography variant="subtitle1" align="left">Meet-up</Typography>
+                <Typography variant="subtitle2" align="left">{location}</Typography>
+                <Link variant="subtitle2" onClick={ onClickChat }>Contact {telegram}</Link>
+            </DialogContainer>
+        </Dialog>
+    );
+}
---
+++ client/src/components/ItemDetails/ImageList.js
@@ -0,0 +1,72 @@
+import { Card, Stack } from "@mui/material";
+import styled from "styled-components";
+import { theme } from "../Theme";
+
+const ImageStack = styled(Stack)`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: stretch;
+    align-self: stretch;
+    height: 30vh;
+    column-gap: 1ch;
+    overflow-x: auto;
+    overflow-y: hidden;
+    padding: 1ch;
+    border: 1px solid ${theme.palette.primary.main};
+    border-radius: 4px;
+`;
+const ImageCard = styled(Card)`
+    display: flex;
+    flex: 0 0 auto;
+    flex-direction: column;
+    width: 30%;
+    & .MuiCardActionArea-root {
+        align-self: stretch;
+        display: flex;
+        flex-direction; column;
+        min-height: 100%;
+        align-items: stretch;
+        justify-content: center;
+        & .MuiCardMedia-root {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    }
+    & .MuiCardActions-root {
+        align-self: stretch;
+        display: flex;
+        flex: 0 1 auto;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+`;
+const ImageDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex: 1 1 0;
+    min-height: 0;
+    width: 100%;
+    img {
+        min-height: 0;
+        object-fit: contain;
+    }
+`;
+
+export default function ImageList(props) {
+    const { imageUrls } = props;
+
+    return (
+        <ImageStack direction="row">
+            { imageUrls.map((imageUrl, index) => {
+                return (
+                    <ImageDiv key={index}>
+                        <img src={imageUrl} alt="Item image" />
+                    </ImageDiv>
+                );
+            })}
+        </ImageStack>
+    );
+}
