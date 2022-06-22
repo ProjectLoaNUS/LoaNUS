ac2140179fb9b189c8cbea817d3a96faee7a851e
Wang Haoyang <haoyangw@gmail.com>
2022-06-22 22:29:27 +0800

    Add a button to SignInPage to go back to home page

--- client/src/pages/SignInPage.js
+++ client/src/pages/SignInPage.js
@@ -1,4 +1,5 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import AppIcon from "../components/AppBtn/AppIcon";
 import AuthCard from "../components/SignInComps/AuthCard";
@@ -6,11 +7,29 @@ import AuthCard from "../components/SignInComps/AuthCard";
 const ContainerStyle = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
-  gap: 5vh;
+  justify-content: flex-start;
   flex-direction: column;
   min-height: 100vh;
   background-color: aliceblue;
+`;
+
+const TopBar = styled.div`
+  align-self: stretch;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  padding: 1rem;
+  align-items: center;
+  flex: 0 0 auto;
+`;
+
+const BodyContainer = styled.div`
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
 `;
 
 const FlexBox = styled(Box)`
@@ -20,14 +39,24 @@ const FlexBox = styled(Box)`
 `;
 
 function SignInPage() {
+  const navigate = useNavigate();
+
+  const backToHome = () => {
+    navigate("/");
+  }
 
   return (
     <ContainerStyle>
-      <FlexBox>
-        <AppIcon dark={false} iconStyles={{height: "15vh"}}/>
-        <Typography variant="h3">LoaNUS</Typography>
-      </FlexBox>
-      <AuthCard />
+      <TopBar>
+        <Button color="primary" variant="outlined" onClick={backToHome}>Back to Home</Button>
+      </TopBar>
+      <BodyContainer>
+        <FlexBox>
+          <AppIcon dark={false} iconStyles={{height: "15vh"}}/>
+          <Typography variant="h3">LoaNUS</Typography>
+        </FlexBox>
+        <AuthCard />
+      </BodyContainer>
     </ContainerStyle>
   );
 }
