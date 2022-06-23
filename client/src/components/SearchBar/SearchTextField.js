5aba27534499670e2bd0271546d631e7239c29d7
Wang Haoyang <haoyangw@gmail.com>
2022-06-24 01:13:23 +0800

    Check that data from backend is done fetching before passing them to DetailsDialog for display, thus fixing errors

--- client/src/components/SearchBar/SearchTextField.js
+++ client/src/components/SearchBar/SearchTextField.js
@@ -10,6 +10,8 @@ import { CentredGrowDiv } from "../FlexDiv";
 import { theme } from '../Theme';
 import { BACKEND_URL } from "../../database/const";
 import { SEARCH_LISTINGS } from "../../pages/routes";
+import DetailsDialog from "../ItemDetails/DetailsDialog";
+import { Buffer } from 'buffer';
 
 const ContrastIconBtn = styled(IconButton)`
     color: ${theme.palette.primary.contrastText};
@@ -57,6 +59,9 @@ export default function SearchTextField() {
   const [queryText, setQueryText] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [clickResult, setClickResult] = useState({});
+  const [clickResultImgs, setClickResultImgs] = useState([]);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     if (!queryText) {
@@ -87,6 +92,37 @@ export default function SearchTextField() {
     }
   };
 
+  const onClickResult = async (event, newValue, reason) => {
+
+    async function imgsToUrls(images) {
+      let imgs = [];
+      const bins = images.data;
+      bins.forEach((bin, index) => {
+        const binary = Buffer.from(bin);
+        const blob = new Blob([binary.buffer], {type: images.contentType[index]});
+        const url = URL.createObjectURL(blob);
+        imgs[index] = url;
+      });
+      setClickResultImgs(imgs);
+    }
+
+    if (reason === "selectOption") {
+      event.defaultMuiPrevented = true;
+      axios
+        .get(`${BACKEND_URL}/api/search-exact`, {
+          params: {
+            name: newValue
+          },
+        })
+        .then((res) => {
+          imgsToUrls(res.data.result.images);
+          setClickResult(res.data.result);
+        })
+        .catch((err) => console.log(err, "error occured"));
+      setOpen(true);
+    }
+  }
+
   const onChangeSearchField = (event) => {
     const value = event.target.value;
     if (value) {
@@ -96,6 +132,7 @@ export default function SearchTextField() {
   }
 
   return (
+    <>
       <Autocomplete
         freeSolo
         fullWidth
@@ -104,6 +141,7 @@ export default function SearchTextField() {
         id="search"
         loading={loading}
         options={searchResults.map((result) => result.title)}
+        onChange={onClickResult}
         renderInput={(params) => {
           return (
             <StyledSearchField
@@ -122,5 +160,18 @@ export default function SearchTextField() {
               }} />
           )
         }} />
+      <DetailsDialog
+        date={clickResult && clickResult.date}
+        userName={clickResult && clickResult.userName}
+        title={clickResult && clickResult.title}
+        imageUrls={clickResultImgs}
+        category={clickResult && clickResult.category}
+        description={clickResult && clickResult.description}
+        location={clickResult && clickResult.location}
+        telegram={clickResult && clickResult.telegram}
+        deadline={clickResult && clickResult.deadline}
+        open={open}
+        setOpen={setOpen} />
+    </>
   );
 }
