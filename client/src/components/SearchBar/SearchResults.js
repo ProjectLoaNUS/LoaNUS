c848b08a0a4a3e590f22705417c08bde061acfbd
Wang Haoyang <haoyangw@gmail.com>
2022-06-23 18:47:34 +0800

    Fetch both item texts and images from /search backend endpoint and process the images into URLs, but don't display them on the ListingSearchPage yet

--- client/src/components/SearchBar/SearchResults.js
+++ client/src/components/SearchBar/SearchResults.js
@@ -1,26 +1,40 @@
-import { List, ListItem, ListItemText } from "@mui/material";
+import { Grid, List, ListItem, ListItemText } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
+import ListingCard from "../ItemList/ListingCard";
+import Loading from "../../assets/loading.svg";
+import NoImage from "../../assets/no-image.png";
+import { CATEGORIES } from "../NewItem/ItemCategories";
 
 const Container = styled.div`
   height: 20vh;
   width: 70vh;
 `;
+const ResultCard = styled(ListingCard)`
+  height: 100%;
+  width: 100%;
+`;
 
 const SearchResults = (props) => {
   const { resultTexts, resultImages } = props;
+  console.log(resultImages);
+
   return (
-    <Container>
-      <List>
-        {resultTexts && resultTexts.map((text) => {
+    <Grid container>
+        {resultTexts && resultTexts.map((text, index) => {
+          const date = new Date(text.date).toLocaleDateString({}, 
+              {year: 'numeric', month: 'short', day: 'numeric'});
+          const deadline = new Date(text.deadline).toLocaleDateString({}, 
+              {year: 'numeric', month: 'short', day: 'numeric'});
+          const category = CATEGORIES[text.category];
+
           return (
-            <ListItem>
-              <ListItemText primary={text.title} />
-            </ListItem>
+            <Grid item direction="column" alignItems="stretch" justifyContent="center">
+              {text.title}
+            </Grid>
           )
         })}
-      </List>
-    </Container>
+    </Grid>
   );
 };
 
--- client/src/pages/ListingSearchPage.js
+++ client/src/pages/ListingSearchPage.js
@@ -6,6 +6,7 @@ import { useLocation, useSearchParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import SearchResults from "../components/SearchBar/SearchResults";
+import { Buffer } from 'buffer';
 import { BACKEND_URL } from "../database/const";
 
 const Container = styled.div`
@@ -17,6 +18,7 @@ function SearchedListings() {
   const [searchResultsText, setSearchResultsText] = useState([]);
   const [searchResultsImage, setSearchResultsImage] = useState([]);
   const { state } = useLocation();
+
   useEffect(() => {
     const url = `${BACKEND_URL}/search`;
     const query = state ? (state.queryText || "") : "";
@@ -31,7 +33,7 @@ function SearchedListings() {
           },
         })
         .then((res) => {
-          setSearchResultsImage(res.data);
+          binsToImgUrls(res.data);
         })
         .catch((err) => console.log(err, "error occured"));
       // Text details(title, description, etc) of search result items
@@ -49,6 +51,22 @@ function SearchedListings() {
         .catch((err) => console.log(err, "error occured"));
     }
   }, []);
+
+  async function binsToImgUrls(bins) {
+    let imgs = [];
+    bins.forEach((bin, index) => {
+      const datas = bin.images.data;
+      let urls = [];
+      datas.forEach((data, i) => {
+        const binary = Buffer.from(data.data);
+        const blob = new Blob([binary.buffer], {type: bin.images.contentType[i]});
+        const url = URL.createObjectURL(blob);
+        urls[i] = url;
+      });
+      imgs[index] = urls;
+    });
+    setSearchResultsImage(imgs);
+  }
 
   return (
     <Container>
