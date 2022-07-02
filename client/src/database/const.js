bbe6901e81ff89c51da252fe62830e271d86b1ac
Wang Haoyang <haoyangw@gmail.com>
2022-07-02 12:39:43 +0800

    Test a different socket-io deployment URL

--- client/src/database/const.js
+++ client/src/database/const.js
@@ -1,4 +1,4 @@
 export const BACKEND_URL =
   process.env.REACT_APP_BACKEND_URL || "https://loanus.herokuapp.com";
 export const SOCKET_URL =
-  process.env.REACT_APP_SOCKET_URL || "wss://loanus-socketio.herokuapp.com";
+  process.env.REACT_APP_SOCKET_URL || "https://loanus-socketio.herokuapp.com";
