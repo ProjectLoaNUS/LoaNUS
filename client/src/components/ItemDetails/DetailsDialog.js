b353f30277ddc04ec941bd2a359529b109c0f71c
Wang Haoyang <haoyangw@gmail.com>
2022-07-03 16:57:24 +0800

    Use the new DetailsView component to display item details in DetailsDialog instead

--- client/src/components/ItemDetails/DetailsDialog.js
+++ client/src/components/ItemDetails/DetailsDialog.js
@@ -6,6 +6,7 @@ import { TransitionGroup } from "react-transition-group";
 import { forwardRef, useState } from "react";
 import { theme } from "../Theme";
 import { useAuth } from "../../database/auth";
+import DetailsView from "./DetailsView";
 
 const DialogContainer = styled(DialogContent)`
     display: flex;
@@ -36,8 +37,7 @@ const CentredButton = styled(Button)`
 `;
 
 export default function DetailsDialog(props) {
-    const { itemId, date, userName, title, isRequest, category, description, location, telegram, imageUrls, deadline, open, setOpen, onActionDone, buttonAction, buttonText } = props;
-    const telegramUsername = telegram ? telegram.replace("@", "") : "";
+    const { itemId, date, userName, title, category, description, location, telegram, imageUrls, deadline, open, setOpen, onActionDone, buttonAction, buttonText } = props;
     const { user } = useAuth();
     const [ isBtnDisabled, setIsBtnDisabled ] = useState(false);
     const [ isActionError, setIsActionError ] = useState(false);
@@ -50,23 +50,6 @@ export default function DetailsDialog(props) {
         setOpen(false);
     };
 
-    const onClickChat = () => {
-        const telegramUrl = "https://t.me/" + telegramUsername;
-        const chatOnTelegram = window.open(telegramUrl, '_blank', 'noopener,noreferrer');
-        if (chatOnTelegram) {
-            chatOnTelegram.opener = null;
-        }
-    }
-
-    const setError = (isError, helperText) => {
-        setIsActionError(isError);
-        setButtonHelperText(helperText);
-    }
-
-    const setIsButtonEnabled = (isEnabled) => {
-        setIsBtnDisabled(!isEnabled);
-    }
-
     return (
         <Dialog
           open={open}
@@ -74,52 +57,29 @@ export default function DetailsDialog(props) {
           scroll="paper"
           fullWidth={true}
           TransitionComponent={Transition}>
-            <DialogTitle>
-                {imageUrls ? "Item Listing" : "Item Request"}
-                <IconButton
-                  aria-label="close"
-                  onClick={handleClose}
-                  sx={{
-                    position: 'absolute',
-                    right: 8,
-                    top: 8,
-                    color: 'rgba(0, 0, 0, 0.87)',
-                  }}
-                >
-                    <CloseIcon />
-                </IconButton>
-            </DialogTitle>
-            <DialogContainer>
-                <Typography variant="h3" align="left">{ title }</Typography>
-                {imageUrls && <ImageList imageUrls={imageUrls} />}
-                <Row>
-                    <Typography variant="body1" align="left">{date} by</Typography>
-                    <ContrastTypo variant="body1" align="left"> {userName}</ContrastTypo>
-                </Row>
-                <Row>
-                    <Typography variant="body1" align="left">In</Typography>
-                    <ContrastTypo variant="body1" align="left"> {category}</ContrastTypo>
-                </Row>
-                <BoldedTypo variant="h6" align="left">Description</BoldedTypo>
-                <Typography variant="body1" align="left">{description}</Typography>
-                {deadline && (
-                    <>
-                        <BoldedTypo variant="h6" align="left">Return deadline</BoldedTypo>
-                        <Typography variant="body1" align="left">{deadline}</Typography>
-                    </>
-                )}
-                <BoldedTypo variant="h6" align="left">Meet-up</BoldedTypo>
-                <Typography variant="body1" align="left">{location}</Typography>
-                <Link variant="body1" onClick={ onClickChat } color="secondary">Contact {telegram}</Link>
-                <CentredButton disabled={isBtnDisabled} variant="contained" color={isActionError ? "error" : "primary"} onClick={buttonAction(setError, setIsButtonEnabled, setOpen, onActionDone, itemId, user)}>{buttonText}</CentredButton>
-                <TransitionGroup>
-                    { buttonHelperText &&
-                        <Slide direction="right">
-                            <Typography variant="subtitle1" align="center" color={ isActionError ? "error" : "success.main" }>{buttonHelperText}</Typography>
-                        </Slide>
-                    }
-                </TransitionGroup>
-            </DialogContainer>
+            <DetailsView
+              imageUrls={imageUrls}
+              handleClose={handleClose}
+              title={title}
+              date={date}
+              userName={userName}
+              category={category}
+              description={description}
+              deadline={deadline}
+              location={location}
+              telegram={telegram}
+              buttonAction={buttonAction}
+              onActionDone={onActionDone}
+              buttonText={buttonText}
+              buttonHelperText={buttonHelperText}
+              setButtonHelperText={setButtonHelperText}
+              isActionError={isActionError}
+              setIsActionError={setIsActionError}
+              isBtnDisabled={isBtnDisabled}
+              setIsBtnDisabled={setIsBtnDisabled}
+              setOpen={setOpen}
+              itemId={itemId}
+              user={user} />
         </Dialog>
     );
 }
