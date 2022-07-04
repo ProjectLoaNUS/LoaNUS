a4e4c9ae6ad4b1192744c96d57dd73693375de4e
Wang Haoyang <haoyangw@gmail.com>
2022-07-04 22:40:59 +0800

    Frontend: Stop connecting to a separate URL for socket-io server now that socket-io is integrated into our existing backend

--- client/src/database/const.js
+++ client/src/database/const.js
@@ -1,4 +1,2 @@
 export const BACKEND_URL =
   process.env.REACT_APP_BACKEND_URL || "https://loanus.herokuapp.com";
-export const SOCKET_URL =
-  process.env.REACT_APP_SOCKET_URL || "https://loanus-socketio.herokuapp.com";
