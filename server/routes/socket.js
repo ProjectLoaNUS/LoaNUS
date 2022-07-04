0f3b53e5ce5dc394857ab99998109bedf635be4c
Wang Haoyang <haoyangw@gmail.com>
2022-07-04 22:37:35 +0800

    Rewrite socket-io server code as a new route in file 'socket.js' under server/ project

---
+++ server/routes/socket.js
@@ -0,0 +1,55 @@
+const socketServer = (app, server) => {
+    const SOCKET_ORIGIN = process.env.SOCKET_ORIGIN || "http://localhost:3000";
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", SOCKET_ORIGIN);
+        next();
+    });
+    const io = require("socket.io")(server, {
+        cors: {
+            origin: SOCKET_ORIGIN
+        }
+    });
+    console.log("socket-io server initialised");
+
+    let users = [];
+    const addUser = (userId, socketId) => {
+        !users.some((user) => user.userId === userId) &&
+            users.push({ userId, socketId });
+    };
+
+    const removeUser = (socketId) => {
+        users = users.filter((user) => user.socketId !== socketId);
+    };
+    const getUser = (userId) => {
+        return users.find((user) => user.userId === userId);
+    };
+    io.on("connection", (socket) => {
+        //When connect
+        console.log("a user connected");
+        //retrieve userId and socketId from user
+        socket.on("addUser", (userId) => {
+            addUser(userId, socket.id);
+            io.emit("getUsers", users);
+        });
+        //Send and get message
+        socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+            const user = getUser(receiverId);
+            if (user) {  
+            io.to(user.socketId).emit("getMessage", {
+                senderId,
+                text,
+            });
+            } else {
+            console.log(`Error: Invalid receiver ID ${receiverId}`);
+            }
+        });
+        //When disconnect
+        socket.on("disconnect", () => {
+            console.log("a user disconnected");
+            removeUser(socket.id);
+            io.emit("getUsers", users);
+        });
+    });
+};
+
+module.exports = socketServer;
