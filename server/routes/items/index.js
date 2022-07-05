a71d48532e78f612d5da5ac163b81dc35dc0eab5
Wang Haoyang <haoyangw@gmail.com>
2022-07-05 16:52:52 +0800

    Import the new routes/items/search.js endpoints under the /api/items route

--- server/routes/items/index.js
+++ server/routes/items/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const listings = require("./itemListings");
 const requests = require("./itemRequests");
+const search = require("./search");
 
 router.use("/items", listings)
-      .use("/items", requests);
+      .use("/items", requests)
+      .use("/items", search);
 
 module.exports = router;
