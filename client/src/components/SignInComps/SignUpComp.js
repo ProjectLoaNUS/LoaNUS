3e62aa5271a35a814f1bcb5138dc4e8196f07244
YongbinWang <yongbin_01@hotmail.com>
2022-07-12 23:54:44 +0800

    Added more tests

--- client/src/components/SignInComps/SignUpComp.js
+++ client/src/components/SignInComps/SignUpComp.js
@@ -4,29 +4,31 @@ export const signUpTitle = "Create an account";
 export const signUpBtnText = "Sign Up";
 
 export default function SignUpComp(props) {
-    const { setName, setAge, showSignUp } = props;
+  const { setName, setAge, showSignUp } = props;
 
-    return (
-        <>
-            <FormControl required variant="outlined" fullWidth>
-                <InputLabel htmlFor="name">Your full name</InputLabel>
-                <OutlinedInput
-                required={showSignUp}
-                id="name"
-                variant="outlined"
-                label="Your full name"
-                onChange={(event) => setName(event.target.value)} />
-            </FormControl>
-            <FormControl required variant="outlined" fullWidth>
-                <InputLabel htmlFor="age">Your age</InputLabel>
-                <OutlinedInput
-                required
-                id="age"
-                variant="outlined"
-                label="Your age"
-                type="number"
-                onChange={(event) => setAge(event.target.value)} />
-            </FormControl>
-        </>
-    );
+  return (
+    <>
+      <FormControl required variant="outlined" fullWidth>
+        <InputLabel htmlFor="name">Your full name</InputLabel>
+        <OutlinedInput
+          required={showSignUp}
+          id="name"
+          variant="outlined"
+          label="Your full name"
+          onChange={(event) => setName(event.target.value)}
+        />
+      </FormControl>
+      <FormControl required variant="outlined" fullWidth>
+        <InputLabel htmlFor="age">Your age</InputLabel>
+        <OutlinedInput
+          required
+          id="age"
+          variant="outlined"
+          label="Your age"
+          type="number"
+          onChange={(event) => setAge(event.target.value)}
+        />
+      </FormControl>
+    </>
+  );
 }
