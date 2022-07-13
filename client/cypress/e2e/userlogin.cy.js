16f4ed006ce362e4b3a8eedd0e2eeebd1e4fe40f
YongbinWang <yongbin_01@hotmail.com>
2022-07-14 00:32:25 +0800

    Creating user end to end cases

---
+++ client/cypress/e2e/userlogin.cy.js
@@ -0,0 +1,25 @@
+describe(" user log in", async () => {
+  it("user signs in", () => {
+    //visit website homepage
+    cy.visit("http://localhost:3000");
+    //sign in button
+    cy.findByRole("link", { name: /sign in/i }).click();
+    //signs in after verification
+    cy.findByRole("textbox", { name: /email/i }).type("yongbin@gmail.com");
+    cy.findByRole("button", { name: /next/i }).click();
+    cy.get("[id=password1]").type("P@ssword1234");
+    cy.get("[id=submit]").click();
+
+    cy.findByRole("button", {
+      name: /wang haoyang jul 6, 2022 item listing image overcooked/i,
+    })
+      .click()
+      .should("be.visible");
+    /* cy.findByRole("button", { name: /borrow it!/i }).click();
+    // not signed in user clicks chat
+    cy.findByRole("button", {
+      name: /wang haoyang jul 6, 2022 item listing image overcooked/i,
+    }).click();*/
+    cy.findByRole("button", { name: /chat/i }).click();
+  });
+});
--- client/src/components/ItemDetails/DetailsDialog.js
+++ client/src/components/ItemDetails/DetailsDialog.js
@@ -8,94 +8,108 @@ import axios from "axios";
 import { BACKEND_URL } from "../../database/const";
 
 const GrowUp = styled(Grow)`
-    transform-origin: bottom center;
+  transform-origin: bottom center;
 `;
 const Transition = forwardRef(function Transition(props, ref) {
-    return <GrowUp ref={ref} {...props} />;
+  return <GrowUp ref={ref} {...props} />;
 });
 const StyledDialog = styled(Dialog)`
-    & .MuiDialog-paper {
-        overflow-y: hidden;
-    }
+  & .MuiDialog-paper {
+    overflow-y: hidden;
+  }
 `;
 
 export default function DetailsDialog(props) {
-    const { itemId, date, owner, title, category, description, location, imageUrls, deadline, open, setOpen, onActionDone, buttonAction, buttonText } = props;
-    const { user } = useAuth();
-    const [ isBtnDisabled, setIsBtnDisabled ] = useState(false);
-    const [ isActionError, setIsActionError ] = useState(false);
-    const [ buttonHelperText, setButtonHelperText ] = useState("");
-    const [ isDetailsView, setIsDetailsView ] = useState(true);
-    const [ chat, setChat ] = useState(null);
+  const {
+    itemId,
+    date,
+    owner,
+    title,
+    category,
+    description,
+    location,
+    imageUrls,
+    deadline,
+    open,
+    setOpen,
+    onActionDone,
+    buttonAction,
+    buttonText,
+  } = props;
+  const { user } = useAuth();
+  const [isBtnDisabled, setIsBtnDisabled] = useState(false);
+  const [isActionError, setIsActionError] = useState(false);
+  const [buttonHelperText, setButtonHelperText] = useState("");
+  const [isDetailsView, setIsDetailsView] = useState(true);
+  const [chat, setChat] = useState(null);
 
-    const handleClose = () => {
-        setButtonHelperText("");
-        setIsActionError(false);
-        setIsBtnDisabled(false);
-        setIsDetailsView(true);
-        setOpen(false);
+  const handleClose = () => {
+    setButtonHelperText("");
+    setIsActionError(false);
+    setIsBtnDisabled(false);
+    setIsDetailsView(true);
+    setOpen(false);
+  };
+
+  const fetchChat = async () => {
+    let convoUsers = {
+      senderId: user?.id,
+      receiverId: owner.id,
     };
+    try {
+      axios.post(`${BACKEND_URL}/api/conversations`, convoUsers).then((res) => {
+        setChat(res.data);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    const fetchChat = async () => {
-        let convoUsers = {
-          senderId: user.id,
-          receiverId: owner.id,
-        };
-        try {
-          axios.post(
-            `${BACKEND_URL}/api/conversations`,
-            convoUsers
-          )
-          .then(res => {
-            setChat(res.data);
-          });
-        } catch (err) {
-          console.log(err);
-        }
-    };
+  useEffect(() => {
+    open && owner && fetchChat();
+  }, [open, owner]);
 
-    useEffect(() => {
-        (open && owner) && fetchChat();
-    }, [open, owner]);
-
-    return (
-        <StyledDialog
-          open={open}
-          onClose={handleClose}
-          fullWidth={true}
-          TransitionComponent={Transition}>
-            { isDetailsView ? 
-                <DetailsView
-                  imageUrls={imageUrls}
-                  handleClose={handleClose}
-                  title={title}
-                  date={date}
-                  owner={owner}
-                  category={category}
-                  description={description}
-                  deadline={deadline}
-                  location={location}
-                  buttonAction={buttonAction}
-                  onActionDone={onActionDone}
-                  buttonText={buttonText}
-                  buttonHelperText={buttonHelperText}
-                  setButtonHelperText={setButtonHelperText}
-                  isActionError={isActionError}
-                  setIsActionError={setIsActionError}
-                  isBtnDisabled={isBtnDisabled}
-                  setIsBtnDisabled={setIsBtnDisabled}
-                  setOpen={setOpen}
-                  itemId={itemId}
-                  user={user}
-                  openChat={() => setIsDetailsView(false)} />
-            :
-                <ChatView
-                  user={user}
-                  owner={owner}
-                  chat={chat}
-                  backToDetails={() => setIsDetailsView(true)}
-                  handleClose={handleClose} />
-            }
-        </StyledDialog>
-    );
+  return (
+    <StyledDialog
+      open={open}
+      onClose={handleClose}
+      fullWidth={true}
+      TransitionComponent={Transition}
+    >
+      {isDetailsView ? (
+        <DetailsView
+          imageUrls={imageUrls}
+          handleClose={handleClose}
+          title={title}
+          date={date}
+          owner={owner}
+          category={category}
+          description={description}
+          deadline={deadline}
+          location={location}
+          buttonAction={buttonAction}
+          onActionDone={onActionDone}
+          buttonText={buttonText}
+          buttonHelperText={buttonHelperText}
+          setButtonHelperText={setButtonHelperText}
+          isActionError={isActionError}
+          setIsActionError={setIsActionError}
+          isBtnDisabled={isBtnDisabled}
+          setIsBtnDisabled={setIsBtnDisabled}
+          setOpen={setOpen}
+          itemId={itemId}
+          user={user}
+          openChat={() => setIsDetailsView(false)}
+        />
+      ) : (
+        <ChatView
+          user={user}
+          owner={owner}
+          chat={chat}
+          backToDetails={() => setIsDetailsView(true)}
+          handleClose={handleClose}
+        />
+      )}
+    </StyledDialog>
+  );
 }
