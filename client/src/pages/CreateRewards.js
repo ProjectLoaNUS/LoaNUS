c39c20f8a0e5a330d25403127d1f9331b1fb5073
Wang Haoyang <haoyangw@gmail.com>
2022-07-15 13:11:27 +0800

    Replace the old create reward form in CreateRewards page with the new MUI CreateRewardCard component

--- client/src/pages/CreateRewards.js
+++ client/src/pages/CreateRewards.js
@@ -1,103 +1,36 @@
 import styled from "styled-components";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import ButtonComponent from "../components/Button";
-import { BACKEND_URL } from "../database/const";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../database/auth";
 import NavigationBar from "../components/NavBar/NavigationBar";
+import CreateRewardCard from "../components/CreateReward/CreateRewardCard";
 
 const MainContainer = styled.div`
   height: 100%;
   width: 100%;
 `;
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0.5em;
+`;
 
 function CreateRewardPage() {
   const { user } = useAuth();
-  const [category, setCategory] = useState("");
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [points, setPoints] = useState(0);
-  const [date, setDate] = useState("");
-  const [image, setImage] = useState();
   const navigate = useNavigate();
-  const handleSubmit = async () => {
-    let formData = new FormData();
-    formData.append("deadline", date);
-    formData.append("category", category);
-    formData.append("title", title);
-    formData.append("description", description);
-    formData.append("claimed", false);
-    formData.append("points", points);
-    formData.append("image", image);
 
-    try {  
-      const res = await axios.post(`${BACKEND_URL}/api/reward/createreward`, formData, {
-        headers: { "Content-Type": "multipart/form-data" },
-      });
-      if (res.status !== 200) {
-        const data = await res.json();
-        console.log(data);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
   useEffect(() => {
     if (!user.admin) {
       navigate(-1);
     }
   });
 
-  const handleFileSubmitChange = (event) => {
-    const fileUploaded = event.target.files[0];
-    setImage(fileUploaded);
-  };
-
   return (
     <MainContainer>
-      <NavigationBar></NavigationBar>
-      <form>
-        <input
-          type="text"
-          placeholder="Vouchers/Beverages/Others"
-          onChange={(e) => setCategory(e.target.value)}
-        ></input>
-        <input
-          type="text"
-          placeholder="Title"
-          onChange={(e) => setTitle(e.target.value)}
-        ></input>
-        <input
-          type="text"
-          placeholder="Description"
-          size="50"
-          onChange={(e) => setDescription(e.target.value)}
-        ></input>
-        <input
-          type="number"
-          placeholder="Points"
-          onChange={(e) => setPoints(e.target.value)}
-        ></input>
-        <input
-          type="datetime-local"
-          onChange={(e) => setDate(e.target.value)}
-        ></input>
-        <input
-          type="file"
-          id="choose-image"
-          name="image"
-          accept="image/gif,image/jpeg,image/png,image/svg+xml"
-          onChange={handleFileSubmitChange}
-        />
-        <ButtonComponent
-          text={"Submit"}
-          state="primary"
-          size={"small"}
-          onClick={handleSubmit}
-          setFontsize={"0.45rem"}
-        ></ButtonComponent>
-      </form>
+      <NavigationBar />
+      <ContentContainer>
+        <CreateRewardCard />
+      </ContentContainer>
     </MainContainer>
   );
 }
