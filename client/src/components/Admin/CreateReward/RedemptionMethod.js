afbeff15778f744c136f6a5ff5af1d0f97fca5c4
Wang Haoyang <haoyangw@gmail.com>
2022-07-16 17:02:34 +0800

    Add tooltip to qr code upload button telling the user its purpose in CreateRewards page

--- client/src/components/Admin/CreateReward/RedemptionMethod.js
+++ client/src/components/Admin/CreateReward/RedemptionMethod.js
@@ -1,4 +1,5 @@
-import { Box, Button, Card, CardMedia, FormControl, FormHelperText, Grow, IconButton, InputAdornment, InputLabel, OutlinedInput, Stack, Typography } from "@mui/material";
+import { Box, Button, Card, CardMedia, FormControl, FormHelperText, Grow, IconButton, InputAdornment, InputLabel, OutlinedInput, Stack, Tooltip, Typography, Zoom } from "@mui/material";
+import { tooltipClasses } from '@mui/material/Tooltip';
 import CloseIcon from '@mui/icons-material/Close';
 import UploadFileIcon from '@mui/icons-material/UploadFile';
 import styled from "styled-components";
@@ -6,6 +7,17 @@ import { theme } from "../../Theme";
 import { TransitionGroup } from 'react-transition-group';
 import { useState } from "react";
 
+const MyTooltip = styled(({ className, ...props }) => (
+    <Tooltip {...props} arrow classes={{ popper: className }} />
+))(({ theme }) => ({
+    [`& .${tooltipClasses.arrow}`]: {
+        color: theme.palette.primary.main
+    },
+    [`& .${tooltipClasses.tooltip}`]: {
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.primary.contrastText
+    }
+}));
 const ImageCard = styled(Card)`
   display: flex;
   flex: 0 0 auto;
@@ -116,9 +128,11 @@ export default function RedemptionMethod(props) {
                   value={redeemUrl}
                   endAdornment={
                     <InputAdornment position="end">
-                        <Button sx={{padding: "16.5px 0"}} onClick={handleChooseQrCode}>
-                            <UploadFileIcon />
-                        </Button>
+                        <MyTooltip TransitionComponent={Zoom} title="Upload QR Code">
+                            <Button sx={{padding: "16.5px 0"}} onClick={handleChooseQrCode}>
+                                <UploadFileIcon />
+                            </Button>
+                        </MyTooltip>
                     </InputAdornment>
                   }
                   sx={{paddingRight: 0, display: "flex"}} />
