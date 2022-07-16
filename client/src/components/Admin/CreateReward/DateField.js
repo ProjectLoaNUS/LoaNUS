e9cf99884aae769094e92f094abe9d42cc4117eb
Wang Haoyang <haoyangw@gmail.com>
2022-07-16 16:16:20 +0800

    Don't depend on isFormError to show error message for DateField of CreateRewards page so other fields having error don't trigger DateField's error message

--- client/src/components/Admin/CreateReward/DateField.js
+++ client/src/components/Admin/CreateReward/DateField.js
@@ -2,17 +2,21 @@ import { TextField } from '@mui/material';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { useState } from 'react';
 
 export default function DateField(props) {
     const { date, setDate, isFormError, setIsFormError, fullWidth } = props;
+    const [ helperText, setHelperText ] = useState("");
 
     const handleChangeDate = (newDate) => {
         const dateNow = new Date();
         dateNow.setHours(0, 0, 0, 0);
         if (newDate < dateNow) {
             setIsFormError(true);
+            setHelperText("Date cannot be in the past");
         } else {
             setIsFormError(false);
+            setHelperText("");
         }
         setDate(newDate);
     }
@@ -28,8 +32,8 @@ export default function DateField(props) {
                 <TextField
                   {...params}
                   fullWidth={fullWidth || true}
-                  error={isFormError}
-                  helperText={ isFormError ? "Date cannot be in the past" : null } />
+                  error={!!helperText}
+                  helperText={helperText} />
               }
             />
         </LocalizationProvider>
