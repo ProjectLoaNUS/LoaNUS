d3f6e6cace23fcc27006d8ea3a7db28b5ce7712b
Wang Haoyang <haoyangw@gmail.com>
2022-07-17 15:24:21 +0800

    ItemCard: Check whether item listing/request is made by the current user in useEffect hook instead in case user or owner object is not defined yet

--- client/src/components/ItemList/ItemCard.js
+++ client/src/components/ItemList/ItemCard.js
@@ -74,7 +74,7 @@ export default function ItemCard(props) {
   const owner = itemDetails.listedBy;
   const description = itemDetails.description;
   const location = itemDetails.location;
-  const isOwner = isUserListingRelated(user, {listedBy: owner});
+  const [isOwner, setIsOwner] = useState(false);
 
   const handleShowDetails = (event) => {
     setOpen(true);
@@ -96,6 +96,12 @@ export default function ItemCard(props) {
           return borrowAction;
       }
   }
+
+  useEffect(() => {
+    if (user && owner) {
+      setIsOwner(isUserListingRelated(user, {listedBy: owner}));
+    }
+  }, [user, owner]);
 
   useEffect(() => {
     if (!!owner) {
