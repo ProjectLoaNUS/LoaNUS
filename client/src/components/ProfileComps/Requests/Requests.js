539aec85278e2226272f10e8011b821efab46aa1
Wang Haoyang <haoyangw@gmail.com>
2022-07-24 16:17:47 +0800

    Abstract out logic for 'Your Request' tab of ProfilePage into its own component in components/ProfileComps/Requests

--- client/src/components/ProfileComps/Requests/Requests.js
+++ client/src/components/ProfileComps/Requests/Requests.js
@@ -3,12 +3,12 @@ import React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
-import ItemList from "../../ItemList/ItemList";
 import { useState, useEffect } from "react";
 import { useAuth } from "../../../database/auth";
 import { Container } from "@mui/material";
 import { BACKEND_URL } from "../../../database/const";
 import { deleteRequestAction } from "../../ItemDetails/detailsDialogActions";
+import YourRequests from "./YourRequests";
 
 const MainContainer = styled.div`
   min-height: 100%;
@@ -33,26 +33,7 @@ const MainContainer = styled.div`
     }
   }
 `;
-const RequestsGrid = styled.div`
-  --grid-layout-gap: 1ch;
-  --grid-column-count: 4;
-  --grid-item--min-width: 210px;
 
-  --gap-count: calc(var(--grid-column-count) - 1);
-  --total-gap-width: calc(var(--gap-count) * var(--grid-layout-gap));
-  --grid-item--max-width: calc((100% - var(--total-gap-width)) / var(--grid-column-count));
-  --grid-item-width: max(var(--grid-item--min-width), var(--grid-item--max-width));
-
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(var(--grid-item-width), 1fr));
-  grid-auto-rows: calc(var(--grid-item-width) * 2 / 3);
-  grid-gap: var(--grid-layout-gap);
-  align-items: stretch;
-  justify-items: stretch;
-  padding: 1ch;
-  height: 100%;
-  overflow-y: auto;
-`;
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -112,15 +93,11 @@ function Requests() {
         {/*<Tab label="Requests for approval"></Tab>*/}
       </Tabs>
       <TabPanel value={selectedTab} index={0} sx={{flex: selectedTab === 0 ? "1 1 auto" : "0 0 0"}}>
-        <ItemList
-          ListContainer={RequestsGrid}
+        <YourRequests
           isLoading={isLoading}
-          buttonText="Delete request"
-          noItemsText="No item requests yet. Create one?"
-          itemDatas={requests}
-          setItemDatas={setRequests}
-          isOwnerOnClickAction={deleteRequestAction}
-          isOwnerButtonText="Delete request" />
+          requests={requests}
+          setRequests={setRequests}
+          deleteRequestAction={deleteRequestAction} />
       </TabPanel>
       {/*<TabPanel value={selectedTab} index={1} sx={{flex: selectedTab === 1 ? "1 1 auto" : "0 0 0"}}>
         No Requests currently
