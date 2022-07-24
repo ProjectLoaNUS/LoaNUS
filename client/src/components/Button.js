40bd65ea83bff2c14c77d9242e5f1e4eb5cba142
Wang Haoyang <haoyangw@gmail.com>
2022-07-25 02:19:32 +0800

    Add LoaNUS-styled LoadingButton component

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,3 +1,5 @@
+import LoadingButton from "@mui/lab/LoadingButton";
+import SaveIcon from '@mui/icons-material/Save';
 import { Button } from "@mui/material";
 import styled from "styled-components";
 
@@ -26,7 +28,38 @@ const MyButton = styled(Button)`
     background: white;
   }
 `;
+const MyLoadingButton = styled(LoadingButton)`
+  min-height: 35px;
+  height: ${(props) => props.height ?? `auto`};
+  width: ${(props) => props.width ?? `max-content`};
+  border-radius: 25px;
+  font-size: ${(props) => props.setfontsize ?? `12`};
+  border: solid 0.1rem transparent;
+  box-shadow: ${(props) =>
+    props.state === "primary" ? `0px` : `2px 1000px 1px #fff inset`};
+  padding: ${(props) => (props.size === "small" ? `7px 14px` : `16px 30px`)};
+  line-height: 1px;
+  white-space: nowrap;
+`;
 function ButtonComponent(props) {
+  if (props.loading) {
+    return (
+      <MyLoadingButton
+        loading
+        loadingPosition="start"
+        startIcon={<SaveIcon />}
+        variant={props.variant}
+        state={props.state}
+        type={props.type ?? "button"}
+        width={props.setWidth}
+        height={props.setHeight}
+        size={props.size ?? "medium"}
+        setfontsize={props.setFontsize}
+      >
+        {props.text}
+      </MyLoadingButton>
+    );
+  }
   return (
     <MyButton
       onClick={props.onClick}
