d374946702d470d1e266faf536db93864b7a6c1b
Wang Haoyang <haoyangw@gmail.com>
2022-07-25 16:06:11 +0800

    Move firebase config values to .env and update setup.js to fetch them from there

--- client/src/database/setup.js
+++ client/src/database/setup.js
@@ -1,17 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyD8KqFXSRWUb5ux0tfmFj8WdBAL2KWMsYU",
-    authDomain: "loanus-orbital.firebaseapp.com",
-    projectId: "loanus-orbital",
-    storageBucket: "loanus-orbital.appspot.com",
-    messagingSenderId: "494853607492",
-    appId: "1:494853607492:web:93d97cbb9ff18425ebac11",
-    measurementId: "G-57EK5B21ZZ"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+let firebaseConfig = {};
+let app;
+export let auth;
+try {
+    if (process.env.REACT_APP_FIREBASE_CONFIG) {
+        firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
+        app = initializeApp(firebaseConfig);
+        auth = getAuth(app);
+    }
+} catch (err) {
+    console.log(err);
+}
